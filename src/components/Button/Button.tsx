1013913e71d49bb8dcf7a99256975fa5d7133735
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-07-21 01:14:53 +0530

    add click handler

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
+import { MouseEventHandler } from "react";
+
 type Button = {
   children: JSX.Element;
   text: string;
+  handleClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ children, text }: Button) {
+export default function Button({ children, text, handleClick }: Button) {
   return (
-    <button className="bg-red border-4 border-peach px-5 py-4 flex gap-1 items-center">
+    <button className="bg-red border-4 border-peach px-5 py-4 flex gap-1 items-center" onClick={handleClick}>
       {children}
       <span className="font-extrabold text-peach text-3xl">{text}</span>
     </button>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,18 +5,21 @@ import Button from "../../components/Button/Button";
 export default function Home() {
   const navigate = useNavigate();
 
+  function handleConnect() {
+
+  }
+
   function handlePlay() {
     navigate("/create-player");
   }
 
-
   return (
-    <div className="flex items-center justify-center flex-col gap-5 w-full h-full" onClick={handlePlay}>
-      <Button text="Play">
-        <Play className="fill-peach h-10 w-16" />
-      </Button>
-      <Button text="Options">
+    <div className="flex items-center justify-center flex-col gap-5 w-full h-full">
+      <Button text="Connect Wallet" handleClick={handleConnect}>
         <Settings className="fill-peach h-10 w-16" />
+      </Button>
+      <Button text="Play" handleClick={handlePlay}>
+        <Play className="fill-peach h-10 w-16" />
       </Button>
     </div>
   );
